90e3bf326936d3e620b2fb73c13b6ad8ccb37ec0
Lao <laos0524@my.uwstout.edu>
2019-12-27 14:08:47 -0600

    Added character controller and modified DestroyPrefab script

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int speed = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey (KeyCode.A)) {
+            transform.Translate(Vector3.left * Time.deltaTime * speed, Space.Self); //LEFT
+        }
+
+        if (Input.GetKey (KeyCode.D)) {
+            transform.Translate(Vector3.right * Time.deltaTime * speed, Space.Self); //RIGHT
+        }
+    }
+}
