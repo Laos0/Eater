584777c6eecbf63648e7bb9bdfc0bba8b06568e2
Lao <laos0524@my.uwstout.edu>
2020-01-08 23:15:58 -0600

    allow character only within camera screen

---
+++ Assets/scripts/ScreenBoundary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBoundary : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        pos.x = Mathf.Clamp01(pos.x);
+        pos.y = Mathf.Clamp01(pos.y);
+        transform.position = Camera.main.ViewportToWorldPoint(pos);
+    }
+}
