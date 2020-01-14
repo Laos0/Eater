c47e0e09af60510a434c5308617470c7ebf8800d
Lao <laos0524@my.uwstout.edu>
2020-01-13 19:14:34 -0600

    combo counter

---
+++ Assets/scripts/ComboCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ComboCounter : MonoBehaviour
+{
+
+    int comboCount;
+    Text comboCountTxt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        comboCount = 0;
+        comboCountTxt.text = comboCount.ToString();
+    }
+
+    void increaseComboCount() {
+
+        comboCount++;
+        comboCountTxt.text = comboCount.ToString();
+    }
+}
